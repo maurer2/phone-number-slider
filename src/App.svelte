7c5f45020dec42388567a50880aa432d92543850
Martin <mr.mmaurer@gmail.com>
2024-12-16 00:10:46 +0100

    Element overflow WIP

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+  const smallestNumber = 1;
+  const largestNumber = 999_999_999;
   const numberFormatter = Intl.NumberFormat('en-GB', {
     minimumIntegerDigits: 9,
     maximumFractionDigits: 0,
     useGrouping: true,
   });
-
-  const handleSubmit = (event: SubmitEvent) => {
-    event.preventDefault();
-  };
+  // const list = Array(100_000);
+  const containerWidth = Math.trunc(largestNumber * 1000); // 16px thumb slider
 
   const formatNumberWithSeparators = (value: number) => {
     const numberParts = numberFormatter.formatToParts(value);
@@ -25,24 +25,24 @@
     return numberFinal;
   };
 
-  let phoneNumber = $state(555_123_123);
+  const handleSubmit = (event: SubmitEvent) => {
+    event.preventDefault();
+  };
 
-  const smallestNumber = 1;
-  const largestNumber = 999_999_999;
-  // const list = Array(100_000);
+  let phoneNumber = $state(0);
 </script>
 
-<main class="container m-auto bg-[#BDE0FE] h-dvh">
+<main class="container m-auto bg-[#BDE0FE] h-dvh p-4">
   <form aria-label="Contact form" onsubmit={handleSubmit}>
-    <div class="grid gap-4 grid-cols-[auto_1fr] p-4">
+    <div class="grid gap-4 grid-cols-[auto_1fr] mb-8">
       <label for="phone-number">Phone number: </label>
-      <div class="overflow-visible grow-1">
+      <div class="overflow-x-scroll grow-1">
         <input
           type="range"
           id="phone-number"
           name="phone-number"
           list="markers"
-          class="w-full"
+          style:width="{containerWidth}px"
           min={smallestNumber}
           max={largestNumber}
           bind:value={phoneNumber}
@@ -59,8 +59,6 @@
         >
       </p>
     </div>
-
-    <hr />
 
     <button type="button">Submit</button>
   </form>
